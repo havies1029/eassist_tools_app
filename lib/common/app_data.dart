3e671d8f287f78d69c5168bf25257bb1cb7e19d6
mickyng <micky@smartsoft-id.com>
2025-03-31 16:23:45 +0700

    kembalikan app_data -> api domain

--- lib/common/app_data.dart
+++ lib/common/app_data.dart
@@ -15,18 +15,20 @@ class AppData {
   static String version = "1.0.2";
 
 
+/*
   static bool useSSL = false;
   static String apiDomain = "http://10.0.2.2/eAssistToolsAPI/";
   static String prefixEndPoint = "/eAssistToolsAPI";
   static String httpAuthority = "10.0.2.2";
+*/
 
-/*
+
   static bool useSSL = true;
   static String apiDomain =
        "http${useSSL ? "s" : ""}://eassisttoolsapi.smartsoft-id.com/";
   static String prefixEndPoint = "";
   static String httpAuthority = "eassisttoolsapi.smartsoft-id.com";
-*/
+
 
 /*
 static bool useSSL = false;
