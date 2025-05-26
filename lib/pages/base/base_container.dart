decab5cdb888d151e4369dc78af5a8701024e83d
unknown <okdiantoardi9@gmail.com>
2025-05-26 09:10:53 +0700

    membuat file find_insurance

--- lib/pages/base/base_container.dart
+++ lib/pages/base/base_container.dart
@@ -1,4 +1,5 @@
 import 'package:eassist_tools_app/pages/chatting/roomcari_list.dart';
+import 'package:eassist_tools_app/pages/find_insurance/find_insurance_main.dart';
 import 'package:eassist_tools_app/pages/groupchat/groupchat_page.dart';
 import 'package:eassist_tools_app/pages/heropage/hero_main.dart';
 import 'package:eassist_tools_app/pages/home/home_page.dart';
@@ -147,7 +148,7 @@ class PageContainer extends PageContainerBase {
 
     switch (pageType) {
       case PageType.home:
-        page = const HeroMain();
+        page = const FindInsuranceMain();
         break;
       case PageType.groupchat:
         page = const ChatPage(roomId: "support");
