af17ca9220982a9cdc7d6bbb30c3bf17acd810c4
mickyng <micky@smartsoft-id.com>
2025-05-31 20:01:29 +0700

    revisi profile general dg penambahan jenis kelamin

---
+++ lib/repositories/gen_profile/rekangeneral_repository.dart
@@ -0,0 +1,21 @@
+import 'package:eassist_tools_app/models/responseAPI/returndataapi_model.dart';
+import 'package:eassist_tools_app/apis/gen_profile/rekangeneral_api.dart';
+import 'package:eassist_tools_app/models/gen_profile/rekangeneral_model.dart';
+
+class RekanGeneralRepository {
+
+	RekanGeneralAPI api = RekanGeneralAPI();
+
+	Future<ReturnDataAPI> rekanGeneralTambah(RekanGeneralModel record) async {
+		return await api.rekanGeneralTambahAPI(record);
+	}
+	Future<bool> rekanGeneralUbah(RekanGeneralModel record) async {
+		return await api.rekanGeneralUbahAPI(record);
+	}
+	Future<bool> rekanGeneralHapus(String mrekan1Id) async {
+		return await api.rekanGeneralHapusAPI(mrekan1Id);
+	}
+	Future<RekanGeneralModel> rekanGeneralLihat(String mrekan1Id) async {
+		return await api.rekanGeneralLihatAPI(mrekan1Id);
+	}
+}
