ad384cfbe139e802c9b7aadc9f44169546c25c6f
mickyng <micky@smartsoft-id.com>
2025-10-20 23:13:22 +0700

    revisi tambah reg user, dilanjutkan verifikasi pin atau tidak perlu

--- lib/blocs/reguser/reguser_bloc.dart
+++ lib/blocs/reguser/reguser_bloc.dart
@@ -9,69 +9,71 @@ part 'reguser_event.dart';
 part 'reguser_state.dart';
 
 class RegUserBloc extends Bloc<RegUserEvents, RegUserState> {
-	final RegUserRepository repository;
+  final RegUserRepository repository;
   final AuthenticationBloc authenticationBloc;
-	RegUserBloc({required this.repository, 
-    required this.authenticationBloc}) : super(const RegUserState()) {
-		on<RegUserTambahEvent>(onTambahRegUser);
+  RegUserBloc({required this.repository, required this.authenticationBloc})
+      : super(const RegUserState()) {
+    on<RegUserTambahEvent>(onTambahRegUser);
     on<RegUserUbahEvent>(onUbahRegUser);
     on<RegUserHapusEvent>(onHapusRegUser);
     on<RegUserLihatEvent>(onLihatRegUser);
     on<ValidasiPinHPEvent>(onValidasiPinHP);
+  }
 
-	}
-
-	Future<void> onTambahRegUser(
-		RegUserTambahEvent event, Emitter<RegUserState> emit) async {
-
-		ReturnDataAPI returnData;
-		bool hasFailure = true;
-		emit(state.copyWith(isSaving: true, isSaved: false));
-		returnData = await repository.regUserTambah(event.record);
-    event.record.reguserId = returnData.data;
-		hasFailure = !returnData.success;
+  Future<void> onTambahRegUser(
+      RegUserTambahEvent event, Emitter<RegUserState> emit) async {
+    ReturnDataAPI returnData;
+    bool hasFailure = true;
+    emit(state.copyWith(isSaving: true, isSaved: false));
+    returnData = await repository.regUserTambah(event.record);
+    final String dataString = returnData.data.toString();
+    List<String> info = dataString.split(';');
+    event.record.reguserId = info.isNotEmpty ? info[0] : '';
+    hasFailure = !returnData.success;
     List<String> errors = [];
     if (hasFailure) {
       errors.add(returnData.data);
     }
-		emit(state.copyWith(
-			isSaving: false,
-			isSaved: true,
-      record: event.record,
-      errors: errors,
-			hasFailure: hasFailure));
+    emit(state.copyWith(
+        isSaving: false,
+        isSaved: true,
+        record: event.record,
+        errors: errors,
+        hasFailure: hasFailure));
 
-    if (!hasFailure) {      
-      authenticationBloc.add(
-        RequirePinHPVerification(hpno: event.record.telepon)
-      );
+    if (!hasFailure) {
+      if (info[1] == '0') {
+        authenticationBloc
+            .add(RequirePinHPVerification(hpno: event.record.telepon));
+      }
     }
+  }
 
-	}
+  Future<void> onUbahRegUser(
+      RegUserUbahEvent event, Emitter<RegUserState> emit) async {
+    emit(state.copyWith(isSaving: true, isSaved: false));
+    bool hasFailure = !await repository.regUserUbah(event.record);
+    emit(
+        state.copyWith(isSaving: false, isSaved: true, hasFailure: hasFailure));
+  }
 
-	Future<void> onUbahRegUser(
-		RegUserUbahEvent event, Emitter<RegUserState> emit) async {
-		emit(state.copyWith(isSaving: true, isSaved: false));
-		bool hasFailure = !await repository.regUserUbah(event.record);
-		emit(state.copyWith(isSaving: false, isSaved: true, hasFailure: hasFailure));
-	}
+  Future<void> onHapusRegUser(
+      RegUserHapusEvent event, Emitter<RegUserState> emit) async {
+    emit(state.copyWith(isSaving: true, isSaved: false));
+    bool hasFailure = !await repository.regUserHapus(event.recordId);
+    emit(
+        state.copyWith(isSaving: false, isSaved: true, hasFailure: hasFailure));
+  }
 
-	Future<void> onHapusRegUser(
-		RegUserHapusEvent event, Emitter<RegUserState> emit) async {
-		emit(state.copyWith(isSaving: true, isSaved: false));
-		bool hasFailure = !await repository.regUserHapus(event.recordId);
-		emit(state.copyWith(isSaving: false, isSaved: true, hasFailure: hasFailure));
-	}
-
-	Future<void> onLihatRegUser(
-		RegUserLihatEvent event, Emitter<RegUserState> emit) async {
-		emit(state.copyWith(isLoading: true, isLoaded: false));
-		RegUserModel record = await repository.regUserLihat(event.recordId);
-		emit(state.copyWith(isLoading: false, isLoaded: true, record: record));
-	}
+  Future<void> onLihatRegUser(
+      RegUserLihatEvent event, Emitter<RegUserState> emit) async {
+    emit(state.copyWith(isLoading: true, isLoaded: false));
+    RegUserModel record = await repository.regUserLihat(event.recordId);
+    emit(state.copyWith(isLoading: false, isLoaded: true, record: record));
+  }
 
   Future<void> onValidasiPinHP(
-    ValidasiPinHPEvent event, Emitter<RegUserState> emit) async {
+      ValidasiPinHPEvent event, Emitter<RegUserState> emit) async {
     emit(state.copyWith(isSaving: true, isSaved: false));
     ReturnDataAPI returnData = await repository.validasiPinHP(event.record);
     bool hasFailure = !returnData.success;
@@ -88,10 +90,7 @@ class RegUserBloc extends Bloc<RegUserEvents, RegUserState> {
     ));
 
     if (!hasFailure) {
-      authenticationBloc.add(
-        PhonePinVerified()
-      );
+      authenticationBloc.add(PhonePinVerified());
     }
   }
-
 }
