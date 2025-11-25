49de996270cc3f7af2bc006487f0c66eb3fa1d3c
mickyng <micky@smartsoft-id.com>
2025-11-25 17:38:36 +0700

    gen regother

---
+++ lib/blocs/regother/regother1crud_state.dart
@@ -0,0 +1,44 @@
+part of 'regother1crud_bloc.dart';
+
+class Regother1CrudState extends Equatable {
+
+	final Regother1CrudModel? record;
+	final bool isLoading;
+	final bool isLoaded;
+	final bool isSaving;
+	final bool isSaved;
+	final bool hasFailure;
+	final ComboRMatauangModel? comboRMatauang;
+	const Regother1CrudState(
+		{this.record,
+		this.isLoading = false,
+		this.isLoaded = false,
+		this.isSaving = false,
+		this.isSaved = false,
+		this.hasFailure = false,
+		this.comboRMatauang,
+});
+
+	Regother1CrudState copyWith({
+		Regother1CrudModel? record,
+		bool? isLoading,
+		bool? isLoaded,
+		bool? isSaving,
+		bool? isSaved,
+		bool? hasFailure,
+		ComboRMatauangModel? comboRMatauang,
+	}){
+		return Regother1CrudState(
+			record: record ?? this.record,
+			isLoading: isLoading ?? this.isLoading,
+			isLoaded: isLoaded ?? this.isLoaded,
+			isSaving: isSaving ?? this.isSaving,
+			isSaved: isSaved ?? this.isSaved,
+			hasFailure: hasFailure ?? this.hasFailure,
+			comboRMatauang: comboRMatauang?? this.comboRMatauang,
+		);
+	}
+
+	@override
+	List<Object> get props => [isLoading, isLoaded, isSaving, isSaved, hasFailure];
+}
